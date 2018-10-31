b7b3edf2aa9bcc8003cae5569706354eb609a0c3
Ashutosh Pandey <ashu1710@gmail.com>
2018-10-30 17:35:53 -0700

    Get screen location of crosshair.

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -4,7 +4,18 @@
 
 /// Get world location of linetrace through crosshair. True if hits landscape.
 bool ATankPlayerController::GetSightRayHitLocation(FVector& OutHitLocation) const {
+	// Set the crosshair locatoin.
+	// Deproject the screen position of the crosshair to a world direction.
+	// Line trace along that direction and see what we hit.
+	
 	OutHitLocation = FVector(1.0);
+	int32 ViewportSizeX, ViewportSizeY;
+
+	GetViewportSize(ViewportSizeX, ViewportSizeY);
+
+	FVector2D ScreenLocatoin(this->CrosshairXLocation * ViewportSizeX, this->CrosshairYLocation * ViewportSizeY);
+	
+	UE_LOG(LogTemp, Warning, TEXT("ScreenLocation: %s"), *(ScreenLocatoin.ToString()));
 	return true;
 }
 
@@ -27,7 +38,7 @@ void ATankPlayerController::AimTowardsCrosshair() {
 	FVector HitLocation;
 	if (GetSightRayHitLocation(HitLocation)) {
 		// TODO Tell controlled tank to aim at this location.
-		UE_LOG(LogTemp, Warning, TEXT("HitLocation: %s"), *(HitLocation.ToString()));
+		/*UE_LOG(LogTemp, Warning, TEXT("HitLocation: %s"), *(HitLocation.ToString()));*/
 	}
 	
 }
--- BattleTank/Source/BattleTank/TankPlayerController.h
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -21,6 +21,12 @@ private:
 	// Return an OUT parameter, true if hit landscape.
 	bool GetSightRayHitLocation(FVector& OutHitLocation) const;
 
+	UPROPERTY(EditAnywhere)
+	float CrosshairXLocation = 0.5;
+
+	UPROPERTY(EditAnywhere)
+	float CrosshairYLocation = 0.33333;
+
 public:
 	ATank* GetControlledTank() const;
 	void BeginPlay() override;
